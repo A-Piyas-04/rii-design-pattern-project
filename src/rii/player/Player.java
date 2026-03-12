7a5e607c293fa3eaad7dc38daf43edf8ad437a20
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2026-03-13 02:45:14 +0600

    .

--- src/rii/player/Player.java
+++ src/rii/player/Player.java
@@ -5,10 +5,8 @@ package rii.player;
  * Ensures only one instance of Player exists.
  */
 public class Player {
-    // Private static instance
     private static Player instance;
 
-    // Private constructor
     private Player() {
         System.out.println("Single Player Mode Activated");
     }
